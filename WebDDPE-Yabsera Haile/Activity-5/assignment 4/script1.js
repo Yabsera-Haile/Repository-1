5ae991078a980057f9153f955fd7a50e5c8a5a5a
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2022-01-20 08:08:17 +0300

    Submitted Activity-5

---
+++ WebDDPE-Yabsera Haile/Activity-5/assignment 4/script1.js
@@ -0,0 +1,10 @@
+let formSubmit=() => document.write("Your form Has Been Submitted");
+function request(a)
+{
+    if(a==1)
+     alert("Your Application has been Submitted.")
+    if(a==2)
+     alert("Your Request has been Submitted.")
+    if(a==3)
+     alert("Your Form has been Submitted.")
+}
