db17bebd12b1dc3c0e7698f931320fbe15b59143
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2022-02-07 20:41:38 +0300

    update

--- WebDDPE-Yabsera Haile/Activity 7/rename.js
+++ WebDDPE-Yabsera Haile/Activity 7/rename.js
@@ -13,14 +13,31 @@ function createF()
 rform.prototype.display=function(){
       const parent = document.getElementById('main');
       const child = document.createElement('div');
-      const tb=document.getElementById("ta");
-      const head=document.createTextNode("New Form Added \n");
-      const element1=document.createTextNode("Full Name: "+this.fname);
-      const element2=document.createTextNode("Age: "+this.age);
-      const element3=document.createTextNode("Phone.: "+this.phone);
+      const tb=document.getElementById("bottomofthepage");
+      const head=document.createElement('div');
+      const headt=document.createTextNode("New Form Added \n");
+      const element1=document.createElement('div');
+      const element1t=document.createTextNode("Full Name: "+this.fname);
+      const element2=document.createElement('div');
+      const element2t=document.createTextNode("Age: "+this.age);
+      const element3=document.createElement('div');
+      const element3t=document.createTextNode("Phone.: "+this.phone);
+      head.appendChild(headt);
+      element1.appendChild(element1t);
+      element2.appendChild(element2t);
+      element3.appendChild(element3t);
       child.appendChild(head);
       child.appendChild(element1);
       child.appendChild(element2);
       child.appendChild(element3);
-      parent.replaceChild(child,tb)
+      child.style.backgroundColor="brown";
+      child.style.width="600px";
+      child.style.height="300px";
+      child.style.margin="50px";
+      child.style.padding="20px";
+      child.style.fontSize="50px";
+      child.style.color="white";
+      child.style.fontStyle="bold";
+      child.style.borderRadius="15px";
+        parent.insertBefore(child,tb);
 }
